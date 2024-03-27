686e3c1914b9ce57afd1905a2295b9339e9e320b
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-03-27 13:51:01 +0800

    Merge the modues

--- microsphere-multiactive-spring-cloud/src/main/java/io/microsphere/multiple/active/zone/spring/cloud/loadbalancer/CustomizedLoadBalancerAutoConfiguration.java
+++ microsphere-multiactive-spring-cloud/src/main/java/io/microsphere/multiple/active/zone/spring/cloud/loadbalancer/CustomizedLoadBalancerAutoConfiguration.java
@@ -1,5 +1,6 @@
 package io.microsphere.multiple.active.zone.spring.cloud.loadbalancer;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,9 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty(value = "microsphere.spring.cloud.loadbalancer.customized", havingValue = "true")
+@ConditionalOnClass(name = {
+        "org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient"
+})
 @LoadBalancerClients(defaultConfiguration = CustomizedLoadBalancerClientConfiguration.class)
 public class CustomizedLoadBalancerAutoConfiguration {
 }
